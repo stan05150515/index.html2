54d72f18960fa4e3f008868f68f7029a8dc9f1ee
stan05150515 <stanwu.0515@gmail.com>
2025-04-25 20:28:21 +0800

    Index

---
+++ index.js
@@ -0,0 +1,20 @@
+function openiframe(index){
+    const c = document.getElementsByClassName('show-classdemo')[0];
+    console.log(c);
+
+    switch (index) {
+        case 1:
+            c.innerHTML = '<iframe src="./411410896w2.html" frameborder="1"></iframe>';
+            break;
+        case 2:
+            c.innerHTML = '<iframe src="411410896_card.html" frameborder="1"></iframe>';    
+            break;
+        case 3:
+            c.innerHTML = '<iframe src="blogs_doge.html" frameborder="1"></iframe>';    
+            break;
+        case 4:
+            c.innerHTML = '<iframe src="blogs_期中.html" frameborder="1"></iframe>';    
+            break;        
+    }
+    console.log(index);//控台輸出
+}
